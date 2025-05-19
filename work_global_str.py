951738a246a34f86d30788af38ac31003a3902ce
zakr1 <zakr1995@gmail.com>
2025-05-19 15:07:45 +0300

    datetime change

--- work_global_str.py
+++ work_global_str.py
@@ -20,8 +20,3 @@
 # str = "this is string example....wow!!!"
 # print(str.translate(trantab))
 
-n = int(input())
-i = [0, 1, 2]
-for _ in range(n):
-    print(i)
-    i = [i[1], i[2], i[0] + i[1] + i[2]]
